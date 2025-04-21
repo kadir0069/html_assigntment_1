950ec676ff6864702b9b83be118f74055e1ccd10
Sk Abdul Kadir <skabdulkadir009@gmail.com>
2025-04-21 21:41:14 +0600

    js changed 2

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
-// Type your code below
-let n1 = 8 ;
-let n2 = 9 ;
-let n3 = n1 == n2;
+function showTitle(){
+  alert (document.getElementById('main-title').innerText);
+}
 
-// Don't change the line below
-console.log(`n1 = ${n1}, n2 = ${n2}, n3 = ${n3}`)
+//class=(.)
+//id=(#)
+
+function showMessage(){
+  const message = document.querySelector('.message');
+  alert (message.innerText);
+}
+
+function changeTitle(){
+  const title = document.getElementById('main-title');
+  title.innerText = "Hello World!";
+  alert ('title changed');
+}
