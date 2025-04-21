c5b1f22937ba406ce08fd416c0f94f073b56ec62
Sk Abdul Kadir <skabdulkadir009@gmail.com>
2025-04-21 12:15:47 +0600

    script change

--- script.js
+++ script.js
@@ -1,15 +1,7 @@
-function emailSend(){
+// Type your code below
+let n1 = 8 ;
+let n2 = 9 ;
+let n3 = n1 == n2;
 
-    Email.send({
-        Host : "s1.maildns.net",
-        Username : "skinnerkiera155@gmail.com",
-        Password : "84DED3FFEA12C5D3FB6A0EFE5A3C715BB499",
-        To : 'skabdulkadir503@gmail.com',
-        From : "skinnerkiera155@gmail.com",
-        Subject : "This is the subject",
-        Body : "And this is the body"
-    }).then(
-      message => alert(message)
-    );
-
-}
+// Don't change the line below
+console.log(`n1 = ${n1}, n2 = ${n2}, n3 = ${n3}`)
